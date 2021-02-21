fde0eb76bec63dd2605eb973534879a00cee3f63
philbro <philipbrodovsky@gmail.com>
2021-02-21 20:25:51 +0200

    init

---
+++ src/components/ExampleView.js
@@ -0,0 +1,18 @@
+//
+
+import './ExampleView.css';
+
+function ExampleView(props) {
+	return (
+		<div>
+			<div className="card">
+				<h3>{props.title}</h3>
+				<div>
+					<button onClick={props.onClick}>{props.btnText}</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ExampleView;
---
+++ src/containers/Example.js
@@ -0,0 +1,20 @@
+//
+import { useState } from 'react';
+
+import ExampleView from '../components/ExampleView';
+
+function Example() {
+	const [count, setCount] = useState(0);
+
+	const add = () => setCount(count + 1);
+	const minus = () => setCount(count - 1);
+
+	return (
+		<div>
+			<ExampleView title={'count: ' + count} onClick={add} btnText="add" />
+			<ExampleView title={'count2: ' + count} onClick={minus} btnText="minus" />
+		</div>
+	);
+}
+
+export default Example;
